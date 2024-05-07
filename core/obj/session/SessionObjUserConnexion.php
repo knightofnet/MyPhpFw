a9998ed91d5d1b40cf06faa7bda274a6a66093f1
Aryx <39461001+knightofnet@users.noreply.github.com>
2024-05-07 20:16:46 +0200

    Correction sur la gestion de la session utilisateur

--- core/obj/session/SessionObjUserConnexion.php
+++ core/obj/session/SessionObjUserConnexion.php
@@ -3,6 +3,7 @@
 namespace myphpfw\core\obj\session;
 
 use Doctrine\ORM\EntityManager;
+use myphpfw\core\module\IUserFw;
 use myphpfw\core\MyPhpFwConf;
 use myphpfw\core\utils\lang\ReflectionUtils;
 use myphpfw\core\utils\lang\StringUtils;
@@ -12,6 +13,7 @@ class SessionObjUserConnexion
 {
     use SessionObjUtilsTrait;
 
+    private ?IUserFw $connectedUser;
 
     public function __construct(string $varStart)
     {
@@ -57,10 +59,15 @@ class SessionObjUserConnexion
         }
 
 
-        $ret = $em->getRepository(MyPhpFwConf::$USER_CLASS)->findOneBy(["id" => $this->getUserId(), MyPhpFwConf::$USER_USERAPITOKEN_FIELD_NAME => $this->getUserApiKey()]);
+        if ($this->connectedUser != null) {
+            return $this->connectedUser;
+        }
+
+        $ret = $em->getRepository(MyPhpFwConf::$USER_CLASS)->findOneBy([MyPhpFwConf::$USER_ID_FIELD_NAME => $this->getUserId(), MyPhpFwConf::$USER_USERAPITOKEN_FIELD_NAME => $this->getUserApiKey()]);
         if ($ret == null) {
             $this->disconnect();
         }
+        $this->connectedUser = $ret;
         return $ret;
 
     }
@@ -73,6 +80,7 @@ class SessionObjUserConnexion
     {
         $this->userId = -1;
         $this->isConnected = false;
+        $this->connectedUser = null;
 
         session_destroy();
 
